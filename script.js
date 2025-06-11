6ee3f84ba80d5086f7060f6ab13223239ee3b430
Julio L <88823380+JULLACE@users.noreply.github.com>
2025-06-11 15:04:26 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         RYM YT Filter
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Replace YouTube links with YouTube-Lite links
+// @author       JULLACE
+// @match        https://rateyourmusic.com/song/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=rateyourmusic.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const YTregex = /watch\?v=.+/;
+
+    const replacer = () => {
+        const arr = document.getElementsByClassName('ui_media_link_btn ui_media_link_btn_youtube');
+        for (let item of arr) {
+            item.href = `https://youtube-lite.js.org/#/${item.href.match(YTregex)}`;
+            observer.disconnect();
+        }
+        console.log('runnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn');
+    };
+
+    const observer = new MutationObserver(() => {
+        replacer();
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
